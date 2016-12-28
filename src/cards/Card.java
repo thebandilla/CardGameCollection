17577267bf1866c255d01f6d470d1bbaf67f2f3c
Ryan Bandilla <ryan.a.bandilla@gmail.com>
2016-12-27 21:10:25 -0500

    added card definition

--- src/cards/Card.java
+++ src/cards/Card.java
@@ -1,5 +1,13 @@
 package cards;
 
 public class Card {
-
+	
+	private final Suit suit;
+	private final Denomination denom;
+	
+	public Card (Suit suit, Denomination denom) {
+		this.suit = suit;
+		this.denom = denom;
+	}
+	
 }
