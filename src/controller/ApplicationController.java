c0d0f622a7f4a17a363487ac7204f5ef58359149
Ryan Bandilla <ryan.a.bandilla@gmail.com>
2017-01-18 13:24:26 -0500

    placeholder controllers for the application

---
+++ src/controller/ApplicationController.java
@@ -0,0 +1,8 @@
+package controller;
+
+public class ApplicationController {
+	/*
+	 * This class will be the outermost controller and will deal with the entire application window.
+	 * Implement other controllers for other card games as they come along.
+	 */
+}
---
+++ src/controller/GoFishController.java
@@ -0,0 +1,7 @@
+package controller;
+
+public class GoFishController {
+	/*
+	 * Example class of a controller for a simple card game
+	 */
+}
